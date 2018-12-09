cb0b2ad84e59196db94eada00ee0b3df956fcf14
Duncan McGregor <duncan@oneeyedmen.com>
2018-12-09 11:19:28 +0000

    Remove example projects out of the site

--- ObjectsAndFunctions/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/Users/duncan/Documents/Work/book/build/install/book/bin/book src/part1 ../_posts/2017-05-17-objects-and-functions-in-kotlin-part1.md
-
-/Users/duncan/Documents/Work/book/build/install/book/bin/book src/part2 ../_posts/2017-05-17-objects-and-functions-in-kotlin-part2.md
